64b66cf8233b11e7214bbcc3ec190892fa1b223a
knightndgale <knightndgale@gmail.com>
2022-01-18 00:28:32 +0800

    Update App.js

--- QRScanner-Test/App.js
+++ QRScanner-Test/App.js
@@ -1,4 +1,5 @@
 
+
 import React, { useState, useEffect} from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Image,  FlatList, Pressable, } from 'react-native';
 import { Provider as PaperProvider,  DefaultTheme, TextInput } from 'react-native-paper';
